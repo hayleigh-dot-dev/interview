805c3cbf7eea121cbcacd6f858c6ad96154fd4d9
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2020-06-05 08:17:05 +0100

    :wrench: Create default tailwind config.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  prefix: '',
+  important: false,
+  separator: ':',
+
+  purge: false,
+
+  theme: {
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      }
+    }
+  },
+  variants: {},
+  corePlugins: {},
+  plugins: []
+}
